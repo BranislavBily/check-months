424869a1acd140f94a64af769123b379449886da
Branislav Bilý <branislav.bily@gmail.com>
2022-02-20 10:51:28 +0100

    Initial Commit

---
+++ CheckMonths/CheckMonthsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMonthsApp.swift
+//  CheckMonths
+//
+//  Created by Branislav Bilý on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckMonthsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
